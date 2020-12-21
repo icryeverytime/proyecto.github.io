21f9da8c7151f60ef7a1f5963ac31e450253ecac
icryeverytime <59993796+icryeverytime@users.noreply.github.com>
2020-12-20 18:32:08 -0800

    Checa la contra
    contra

--- validar.php
+++ validar.php
@@ -1,6 +1,6 @@
 <?php
 $key='1234qwerasdfoj2340wqer#2143!23q';
-
+$data="";
 function decryptthis($data, $key) {
     $encryption_key = base64_decode($key);
     list($encrypted_data, $iv) = array_pad(explode('::', base64_decode($data), 2),2,null);
@@ -25,26 +25,22 @@ if ($conn->connect_error) {
   die("Conexion fallo: " . $conn->connect_error);
 }
 
-$usuario =$conn->real_escape_string($_POST['usuario']);
-$contraseña =$conn->real_escape_string($_POST['contraseña']); 
+$usuario =$_POST['usuario'];
+$contraseña =$_POST['contraseña']; 
 
-$sql = "SELECT * FROM ´usuario´ WHERE Correo=$usuario";
-
-
-
+$sql = "SELECT Contra FROM usuario WHERE Correo='$usuario'";
 
 $resultado= $conn->query($sql);
-echo $resultado;
-if($resultado){
-    while($row=mysql_fetch_row($resultado)){
-        echo $row;
-        
+while($row=$resultado->fetch_assoc())
+{
+    $contra=decryptthis($row['Contra'],$key);
+    if($contra==$contraseña)
+    {
+        echo "adentro";
     }
-    
+    else{
+        echo "no adentro0";
+    }
 }
-
-
-
-
 
 ?>
