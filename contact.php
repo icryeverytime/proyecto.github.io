758d377211a11e11222eb0cfb6af820ff626c12c
LordNX <74880769+LordNX@users.noreply.github.com>
2020-12-21 19:48:23 -0600

    visual fix logout btn
    se coloreo el btn de logout y se corrigio el tamaño

--- contact.php
+++ contact.php
@@ -1,178 +1,205 @@
+<?php 
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pagina Principal</title>
-        <link rel="shortcut icon" href="img/favicon.ico"/>
-        <link rel="stylesheet" href="estilos/estilos.css"/>
-        <script src="https://kit.fontawesome.com/cebbaaaaab.js" crossorigin="anonymous"></script>
-        <script src="js/funciones.js"></script>
-        <link rel="stylesheet" href="estilos/estilocontac.css"/>
-    </head>
-    <body>
-        
-    <header>
-  <div id="navbar">
-    <header>
-      <a href="index.php">
-        <img src="img/Logo2.png" class="img-centrar" id="head">
-      </a>
-      <br>
-    </header>
-    <nav>
-      <ul>
 
-        <li><a class="active" id="hide" href="index.php"><br>Inicio</a></li>
-        <li><a href="tienda.php" id="hide"><br>Tienda</a></li>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pagina Principal</title>
+  <link rel="shortcut icon" href="img/favicon.ico" />
+  <link rel="stylesheet" href="estilos/estilos.css" />
+  <script src="https://kit.fontawesome.com/cebbaaaaab.js" crossorigin="anonymous"></script>
+  <script src="js/funciones.js"></script>
+  <link rel="stylesheet" href="estilos/estilocontac.css" />
+</head>
 
-        <li><a href="acercade.php" id="hide"><br>Acerca de</a></li>
-        <li><a href="contact.php" class="active" id="hide"><br>Contáctanos</a></li>
-        <li><a href="faq.php" id="hide"><br>Ayuda</a></li>
+<body>
 
-        <li style="float: right;">
-          <button href="index.php" id="log" role="button" class=""
-            onclick="document.getElementById('id01').style.display='block'">Login</button>
-        </li>
-        
+  <header>
+    <div id="navbar">
+      <header>
+        <a href="index.php">
+          <img src="img/Logo2.png" class="img-centrar" id="head">
+        </a>
+        <br>
+      </header>
+      <nav>
+        <ul>
 
+          <li><a class="active" id="hide" href="index.php"><br>Inicio</a></li>
+          <li><a href="tienda.php" id="hide"><br>Tienda</a></li>
 
+          <li><a href="acercade.php" id="hide"><br>Acerca de</a></li>
+          <li><a href="contact.php" class="active" id="hide"><br>Contáctanos</a></li>
+          <li><a href="faq.php" id="hide"><br>Ayuda</a></li>
 
+          <li style="float: right;">
+            <?php if(empty($_SESSION["usuario"])): ?>
+            <button id="log" onclick="document.getElementById('id01').style.display='block'">Login</button>
+            <?php else: ?>
+          <li style="float: right;"> <button id="log2" onclick="location.href='logout.php'; ">Logout</li>
+          <?php endif; ?>
 
+          </li>
+          <li style="float: right;">
 
-
-        <li onclick="tres()">
-          <img src="img/menu.png" width="40px" height="40px" id="show">
-        </li>
-        <li style="float: right;"><a href="" style="height: 50.8px;"><img src="img/cart.png" width="31px"
-              height="40px"></a></li>
-      </ul>
-    </nav>
-    <div id="mostrar">
-      <a href=# id="muestra" onclick="dos()">X</a>
-
-      <a href="index.php" class="active" id="muestra" style="background-color:  #0466C8;">Inicio</a>
-      <a href="tienda.php" id="muestra">Tienda</a>
-
-      <a href="acercade.php" id="muestra">Acerca de</a>
-      <a href="contact.php" class="active" id="muestra">Contáctanos</a>
-      <a href="faq.php" id="muestra">Ayuda</a>
-    </div>
-  </div>
-</header>
-<contacto>
-        <div class="container">
-	<div class="row">
-			<h1>Contáctanos</h1>
-	</div>
-	<div class="row">
-			<h4 style="text-align:center">Queremos saber tu opinión.</h4>
-	</div>
-	<div class="row input-container">
-			<div class="col-xs-12">
-				<div class="styled-input wide">
-          <form action="">
-					<input type="text" required />
-					<label>Nombre</label> 
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-12">
-				<div class="styled-input">
-        <input type="email" id="email" name="email" size="40" required />	
-          <label>Email</label> 
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-12">
-				<div class="styled-input" style="float:right;">
-        <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" size="40"  required/> 
-					<label>Número de teléfono</label> 
-				</div>
-			</div>
-			<div class="col-xs-12">
-				<div class="styled-input wide">
-					<textarea required></textarea>
-					<label>Asunto</label>
-				</div>
-			</div>
-			<div class="col-xs-12">
-        <button type="submit" name="sendmail" class="btn-lrg submit-btn">Enviar mensaje</button>
-      </div>
-      </form>
-	</div>
-</div>
-</contacto>
-        <footer>
-          <div class="pie">
-             <div>
-               <h2>Sobre nosotros</h2>
-               <a href="acercade.php" class="linkpie">Más información</a>
-             </div>
-             <div>
-               <h2>Síganos</h2>
-               <a href="https://www.facebook.com/Midnight-Profesional-113600053882887"><img src="img/facebook-icon.png" width="40px" height="40px"></a>
-               <a href="https://www.instagram.com/midnightprofessional/"><img src="img/Instagram.png" width="40px" height="40px"></a>
-               <a href="https://twitter.com/MidnightProfes1"><img src="img/Twitter-icon.png" width="40px" height="40px"></a>
-             </div>
-             <div>
-              <h2>Contáctanos</h2>
-              <a href="contact.php" class="linkpie">Formulario de contacto</a>
-             </div>
-          </div>
-          <div class="copyright">
-            <div class="contenido">
-              <img src="img/pagos.png" alt=""><br><br>
-              Copyright © 2020 Midnight Professional
+            <div class="nompadre">
+              <div class="nomhijo">
+                <?php 
+        if(!empty($_SESSION["usuario"])) {
+          echo $_SESSION["usuario"] ; 
+        }
+          ?>
+              </div>
             </div>
-          </div>
-        </footer>
+          </li>
 
 
 
-        <logi>
 
-<div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="validar.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img/logologin.png" alt="Avatar" class="avatar">
+
+
+
+          <li onclick="tres()">
+            <img src="img/menu.png" width="40px" height="40px" id="show">
+          </li>
+          <li style="float: right;"><a href="" style="height: 50.8px;"><img src="img/cart.png" width="31px"
+                height="40px"></a></li>
+        </ul>
+      </nav>
+      <div id="mostrar">
+        <a href=# id="muestra" onclick="dos()">X</a>
+
+        <a href="index.php" class="active" id="muestra" style="background-color:  #0466C8;">Inicio</a>
+        <a href="tienda.php" id="muestra">Tienda</a>
+
+        <a href="acercade.php" id="muestra">Acerca de</a>
+        <a href="contact.php" class="active" id="muestra">Contáctanos</a>
+        <a href="faq.php" id="muestra">Ayuda</a>
+      </div>
     </div>
-
+  </header>
+  <contacto>
     <div class="container">
-      <label for="uname"><b>Usuario</b></label>
-      <input type="text" placeholder="Nombre de usuario" name="usuario" required>
+      <div class="row">
+        <h1>Contáctanos</h1>
+      </div>
+      <div class="row">
+        <h4 style="text-align:center">Queremos saber tu opinión.</h4>
+      </div>
+      <div class="row input-container">
+        <div class="col-xs-12">
+          <div class="styled-input wide">
+            <form action="">
+              <input type="text" required />
+              <label>Nombre</label>
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
+          <div class="styled-input">
+            <input type="email" id="email" name="email" size="40" required />
+            <label>Email</label>
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
+          <div class="styled-input" style="float:right;">
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" size="40" required />
+            <label>Número de teléfono</label>
+          </div>
+        </div>
+        <div class="col-xs-12">
+          <div class="styled-input wide">
+            <textarea required></textarea>
+            <label>Asunto</label>
+          </div>
+        </div>
+        <div class="col-xs-12">
+          <button type="submit" name="sendmail" class="btn-lrg submit-btn">Enviar mensaje</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  </contacto>
+  <footer>
+    <div class="pie">
+      <div>
+        <h2>Sobre nosotros</h2>
+        <a href="acercade.php" class="linkpie">Más información</a>
+      </div>
+      <div>
+        <h2>Síganos</h2>
+        <a href="https://www.facebook.com/Midnight-Profesional-113600053882887"><img src="img/facebook-icon.png"
+            width="40px" height="40px"></a>
+        <a href="https://www.instagram.com/midnightprofessional/"><img src="img/Instagram.png" width="40px"
+            height="40px"></a>
+        <a href="https://twitter.com/MidnightProfes1"><img src="img/Twitter-icon.png" width="40px" height="40px"></a>
+      </div>
+      <div>
+        <h2>Contáctanos</h2>
+        <a href="contact.php" class="linkpie">Formulario de contacto</a>
+      </div>
+    </div>
+    <div class="copyright">
+      <div class="contenido">
+        <img src="img/pagos.png" alt=""><br><br>
+        Copyright © 2020 Midnight Professional
+      </div>
+    </div>
+  </footer>
 
-      <label for="psw"><b>Contraseña</b></label>
-      <input type="password" placeholder="Ingrese contraseña" name="contraseña" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Recordarme
-      </label>
+
+
+  <logi>
+
+    <div id="id01" class="modal">
+
+      <form class="modal-content animate" action="validar.php" method="post">
+        <div class="imgcontainer">
+          <span onclick="document.getElementById('id01').style.display='none'" class="close"
+            title="Close Modal">&times;</span>
+          <img src="img/logologin.png" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+          <label for="uname"><b>Usuario</b></label>
+          <input type="text" placeholder="Nombre de usuario" name="usuario" required>
+
+          <label for="psw"><b>Contraseña</b></label>
+          <input type="password" placeholder="Ingrese contraseña" name="contraseña" required>
+
+          <button type="submit">Login</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember"> Recordarme
+          </label>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+          <button type="button" onclick="document.getElementById('id01').style.display='none'"
+            class="cancelbtn">Cancelar</button>
+          <span class="psw">No tienes cuenta? <a href="registro.html">Suscribete</a></span>
+        </div>
+      </form>
     </div>
 
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancelar</button>
-      <span class="psw">No tienes cuenta? <a href="registro.html">Suscribete</a></span>
-    </div>
-  </form>
-</div>
+    <script>
+      // Get the modal
+      var modal = document.getElementById('id01');
 
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-</logi>
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    </script>
+  </logi>
 
 
 
 
-    </body>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -55,69 +55,68 @@ session_start();
 <body>
 
 
-<header>
-  <div id="navbar">
-    <header>
-      <a href="index.php">
-        <img src="img/Logo2.png" class="img-centrar" id="head">
-      </a>
-      <br>
-    </header>
-    <nav>
-      <ul>
-        <li><a class="active" id="hide" href="index.php"><br>Inicio</a></li>
-        <li><a href="aas.php" id="hide"><br>Tienda</a></li>
-        <li><a href="acercade.php" id="hide"><br>Acerca de</a></li>
-        <li><a href="contact.php" id="hide"><br>Contáctanos</a></li>
-        <li><a href="faq.php" id="hide"><br>Ayuda</a></li>
-        <li style="float: right;">
-        
-        
-    
+  <header>
+    <div id="navbar">
+      <header>
+        <a href="index.php">
+          <img src="img/Logo2.png" class="img-centrar" id="head">
+        </a>
+        <br>
+      </header>
+      <nav>
+        <ul>
+          <li><a class="active" id="hide" href="index.php"><br>Inicio</a></li>
+          <li><a href="aas.php" id="hide"><br>Tienda</a></li>
+          <li><a href="acercade.php" id="hide"><br>Acerca de</a></li>
+          <li><a href="contact.php" id="hide"><br>Contáctanos</a></li>
+          <li><a href="faq.php" id="hide"><br>Ayuda</a></li>
+          <li style="float: right;">
 
 
-<?php if(empty($_SESSION["usuario"])): ?>
-  <button id="log" onclick="document.getElementById('id01').style.display='block'">Login</button>
-<?php else: ?>
-  <li style="float: right;"> <button id="log" onclick="location.href='logout.php'; ">Logout</li>
-<?php endif; ?>
+            <?php if(empty($_SESSION["usuario"])): ?>
+            <button id="log" onclick="document.getElementById('id01').style.display='block'">Login</button>
+            <?php else: ?>
+          <li style="float: right;"> <button id="log2" onclick="location.href='logout.php'; ">Logout</li>
+          <?php endif; ?>
 
-        
-        </li>
-        <li style="float: right;">
-        
-        <div class="nompadre">
-            <div class="nomhijo">
-        <?php 
+          </li>
+          <li style="float: right;">
+
+            <div class="nompadre">
+              <div class="nomhijo">
+                <?php 
         if(!empty($_SESSION["usuario"])) {
           echo $_SESSION["usuario"] ; 
         }
-          ?>   
-          </div>
-        </div>
+          ?>
+              </div>
+            </div>
 
-          
-        <li onclick="tres()">
-          <img src="img/menu.png" width="40px" height="40px" id="show">
-        </li>
-        <li style="float: right;"><a href="" style="height: 50.8px;"><img src="img/cart.png" width="31px"
-              height="40px"></a></li>
-      </ul>
-    </nav>
-    <div id="mostrar">
-      <a href=# id="muestra" onclick="dos()">X</a>
-      <a href="index.php" class="active" id="muestra" style="background-color:  #0466C8;">Inicio</a>
-      <a href="aas.asp" id="muestra">Tienda</a>
-      <a href="acercade.php" id="muestra">Acerca de</a>
-      <a href="contact.php" id="muestra">Contáctanos</a>
-      <a href="faq.php" id="muestra">Ayuda</a>
 
-    </div>
+          <li onclick="tres()">
+            <img src="img/menu.png" width="40px" height="40px" id="show">
+          </li>
+          <li style="float: right;"><a href="" style="height: 50.8px;"><img src="img/cart.png" width="31px"
+                height="40px"></a></li>
+        </ul>
+      </nav>
+      <div id="mostrar">
+        <a href=# id="muestra" onclick="dos()">X</a>
+        <a href="index.php" class="active" id="muestra" style="background-color:  #0466C8;">Inicio</a>
+        <a href="aas.asp" id="muestra">Tienda</a>
+        <a href="acercade.php" id="muestra">Acerca de</a>
+        <a href="contact.php" id="muestra">Contáctanos</a>
+        <a href="faq.php" id="muestra">Ayuda</a>
+
+      </div>
   </header>
 
-<div style="text-align: center; color: #FFF; display: flex; flex-direction: column; align-items: center; justify-content: center;  letter-spacing: 1px;">
- <h1 style="background-image:url(https://media.giphy.com/media/3ornjHyjf1FiUneGGI/giphy.gif); background-size: cover; color: transparent; -moz-background-clip: text; -webkit-background-clip:text; text-transform: uppercase; font-size: 100px; line-height: .75; margin:10px 0;">it's all<br/>about<br/>gaming</h1>
-</div>
+  <div
+    style="text-align: center; color: #FFF; display: flex; flex-direction: column; align-items: center; justify-content: center;  letter-spacing: 1px;">
+    <h1
+      style="background-image:url(https://media.giphy.com/media/3ornjHyjf1FiUneGGI/giphy.gif); background-size: cover; color: transparent; -moz-background-clip: text; -webkit-background-clip:text; text-transform: uppercase; font-size: 100px; line-height: .75; margin:10px 0;">
+      it's all<br />about<br />gaming</h1>
+  </div>
 
   <div class="content">
     <h2 style="text-align: center;">Encuentra todas las consolas de la nueva generación</h2>
@@ -262,48 +261,50 @@ session_start();
 
 
 
-<logi>
+  <logi>
 
-<div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="validar.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img/logologin.png" alt="Avatar" class="avatar">
+    <div id="id01" class="modal">
+
+      <form class="modal-content animate" action="validar.php" method="post">
+        <div class="imgcontainer">
+          <span onclick="document.getElementById('id01').style.display='none'" class="close"
+            title="Close Modal">&times;</span>
+          <img src="img/logologin.png" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+          <label for="uname"><b>Usuario</b></label>
+          <input type="text" placeholder="Nombre de usuario" name="usuario" required>
+
+          <label for="psw"><b>Contraseña</b></label>
+          <input type="password" placeholder="Ingrese contraseña" name="contraseña" required>
+
+          <button type="submit">Login</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember"> Recordarme
+          </label>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+          <button type="button" onclick="document.getElementById('id01').style.display='none'"
+            class="cancelbtn">Cancelar</button>
+          <span class="psw">No tienes cuenta? <a href="registro.html">Suscribete</a></span>
+        </div>
+      </form>
     </div>
 
-    <div class="container">
-      <label for="uname"><b>Usuario</b></label>
-      <input type="text" placeholder="Nombre de usuario" name="usuario" required>
+    <script>
+      // Get the modal
+      var modal = document.getElementById('id01');
 
-      <label for="psw"><b>Contraseña</b></label>
-      <input type="password" placeholder="Ingrese contraseña" name="contraseña" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Recordarme
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancelar</button>
-      <span class="psw">No tienes cuenta? <a href="registro.html">Suscribete</a></span>
-    </div>
-  </form>
-</div>
-
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-</logi>
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    </script>
+  </logi>
 
 
 </body>
